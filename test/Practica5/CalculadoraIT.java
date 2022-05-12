33df2321d0c95aa63ff0b12de41bb9292335e157
juansanchezg <jsanchezgayoso@cifpfbmoll.eu>
2022-05-12 22:59:28 +0200

    Creación tests unitarios

---
+++ test/Practica5/CalculadoraIT.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package Practica5;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jsanc
+ */
+public class CalculadoraIT {
+    
+    public CalculadoraIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of suma method, of class Calculadora.
+     */
+    @Test
+    public void testSuma() {
+        System.out.println("suma");
+        int x = 0;
+        int y = 0;
+        Calculadora instance = new Calculadora();
+        int expResult = 0;
+        int result = instance.suma(x, y);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of resta method, of class Calculadora.
+     */
+    @Test
+    public void testResta() {
+        System.out.println("resta");
+        int x = 0;
+        int y = 0;
+        Calculadora instance = new Calculadora();
+        int expResult = 0;
+        int result = instance.resta(x, y);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of multiplicacion method, of class Calculadora.
+     */
+    @Test
+    public void testMultiplicacion() {
+        System.out.println("multiplicacion");
+        int x = 0;
+        int y = 0;
+        Calculadora instance = new Calculadora();
+        int expResult = 0;
+        int result = instance.multiplicacion(x, y);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of division method, of class Calculadora.
+     */
+    @Test
+    public void testDivision() {
+        System.out.println("division");
+        int x = 0;
+        int y = 0;
+        Calculadora instance = new Calculadora();
+        int expResult = 0;
+        int result = instance.division(x, y);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
