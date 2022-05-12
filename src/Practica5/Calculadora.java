6326a1dc7339586cd887907bb97bbe396404f03d
juansanchezg <jsanchezgayoso@cifpfbmoll.eu>
2022-05-12 22:28:13 +0200

    Calculadora creada

---
+++ src/Practica5/Calculadora.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practica5;
+
+public class Calculadora implements iCalculadora {
+
+    /**
+     * método suma(): operación suma
+     * @param x,y
+     * @return: devuelve resultado de x + y
+     */
+    public int suma(int x, int y) {
+        return x + y;
+    }
+
+    /**
+     * método resta(): operación resta
+     * @param x,y
+     * @return: devuelve resultado de x - y
+     */
+    public int resta(int x, int y) {
+        return x - y;
+    }
+
+    /**
+     * método multiplicacion(): operación multiplicación
+     * @param x,y
+     * @return: devuelve resultado de x * y
+     */
+    public int multiplicacion(int x, int y) {
+        return x * y;
+    }
+
+    /**
+     * método division(): operación división 
+     * @param x,y
+     * @return: devuelve resultado de x / y
+     */
+    public int division(int x, int y) {
+        return x / y;
+    }
+
+}
---
+++ src/Practica5/iCalculadora.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practica5;
+
+public interface iCalculadora {
+
+    /**
+     * declaración método suma()
+     * @param x,y
+     */
+    public abstract int suma(int x, int y);
+
+    /**
+     * declaración método resta()
+     * @param x,y
+     */
+    public abstract int resta(int x, int y);
+
+    /**
+     * declaración método multiplicacion()
+     * @param x,y
+     */
+    public abstract int multiplicacion(int x, int y);
+
+    /**
+     * declaración método division()
+     * @param x,y
+     */
+    public abstract int division(int x, int y);
+}
